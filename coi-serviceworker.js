4f6e46e2e09a7a05e39330818d0946b1fbfa4488
n3yfx <odhranworrallsheesh@gmail.com>
2026-06-12 22:11:35 +0100

    Create coi-serviceworker.js

---
+++ coi-serviceworker.js
@@ -0,0 +1,57 @@
+if (typeof window === 'undefined') {
+    self.addEventListener("install", () => self.skipWaiting());
+    self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+
+    self.addEventListener("message", (ev) => {
+        if (ev.data === "deregister") {
+            self.registration.unregister().then(() => self.clients.matchAll()).then(clients => {
+                clients.forEach((client) => client.navigate(client.url));
+            });
+        }
+    });
+
+    self.addEventListener("fetch", function (event) {
+        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+            return;
+        }
+
+        event.respondWith(
+            fetch(event.request).then((response) => {
+                if (response.status === 0) {
+                    return response;
+                }
+
+                const newHeaders = new Headers(response.headers);
+                newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+                newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+
+                return new Response(response.body, {
+                    status: response.status,
+                    statusText: response.statusText,
+                    headers: newHeaders,
+                });
+            }).catch((e) => console.error(e))
+        );
+    });
+} else {
+    (() => {
+        const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
+        const isHttps = window.location.protocol === 'https:';
+        
+        if (isLocalhost || isHttps) {
+            window.addEventListener('load', () => {
+                navigator.serviceWorker.register(window.document.currentScript.src).then(
+                    (registration) => {
+                        console.log('coi-worker registered', registration.scope);
+                        if (registration.active && !navigator.serviceWorker.controller) {
+                            window.location.reload();
+                        }
+                    },
+                    (err) => {
+                        console.error('coi-worker failed: ', err);
+                    }
+                );
+            });
+        }
+    })();
+}
